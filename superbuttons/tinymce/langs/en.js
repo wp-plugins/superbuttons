833c8bda8f280fa8e08b7e9191906b4f886252f1
ragulka <ragulka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-19 07:51:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/superbuttons/trunk@230646 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- superbuttons/tinymce/langs/en.js
+++
@@ -1,9 +0,0 @@
-tinyMCE.addI18n('en.superbuttons',{
-desc:"Create A SuperButton",
-superbutton_title:"Create a SuperButton",
-text:"Button Text",
-button_link:"Link",
-title:"Title",
-image_url:"Image URL",
-style:"Style"
-});
--- superbuttons/tinymce/langs/et.js
+++
@@ -1,9 +0,0 @@
-tinyMCE.addI18n('en.superbuttons',{
-desc:"Sisesta SuperButton",
-superbutton_title:"Sisesta SuperButton",
-text:"Nupu tekst",
-button_link:"Link",
-title:"Tiitel",
-image_url:"Pildi URL",
-style:"Stiil"
-});
