5520f87967aeba456979a7c6bfe547424deb68e9
ragulka <ragulka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-23 09:15:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/superbuttons/tags/0.5.1@232304 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tinymce/langs/et.js
@@ -0,0 +1,9 @@
+tinyMCE.addI18n('en.superbuttons',{
+desc:"Sisesta SuperButton",
+superbutton_title:"Sisesta SuperButton",
+text:"Nupu tekst",
+button_link:"Link",
+title:"Tiitel",
+image_url:"Pildi URL",
+style:"Stiil"
+});
